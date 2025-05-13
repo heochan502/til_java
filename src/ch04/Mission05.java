b4980dc3e081f0ca3978b46f1f4b6f52d98deec0
greenhc502 <greenhc502@gmail.com>
2025-05-13 16:20:08 +0900

    0513 반복문 for문2

--- src/ch04/Mission05.java
+++ src/ch04/Mission05.java
@@ -5,9 +5,56 @@ public class Mission05 {
     public static void main(String[] args)
     {
         int starCount = (int)(Math.random() * 6.0) + 2; // 1) 2~7사이 랜덤값
-        for (int i=0 ; i<10; i++){
-            System.out.printf("starCount : %d\n", starCount);
-            starCount = (int)(Math.random() * 6.0) + 2;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("별의 갯수를 입력해주세요.:");
+        int Star_num = scan.nextInt();
+
+        String _star="*";
+/*
+        for (int i =1 ; i<Star_num; i++ )
+        {
+            System.out.printf("%s",_star);
         }
+        System.out.println();
+
+        for (int i = Star_num ; i>0; i-- )
+        {
+            System.out.printf("%s",_star);
+        }
+        System.out.println();
+*/
+
+
+        for (int i = 0 ; i<=Star_num; i++ )
+        {
+            for (int j =0; j<i;j++){
+            System.out.printf("%s",_star);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0 ; i<=Star_num; i++ )
+        {
+            for (int j =0; j<i;j++){
+                System.out.printf("%s",_star);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+//        for (int i=0; i<10; i++)
+//        {
+//            for (int j=0; j<starCount; j++)
+//            {
+//                System.out.printf("%s", _star);
+//            }
+//            System.out.println();
+//            starCount = (int)(Math.random() * 6.0) + 2;
+//        }
+
+
+
     }
 }
---
+++ src/ch04/Mission06.java
@@ -0,0 +1,39 @@
+package ch04;
+import java.util.Scanner;
+
+public class Mission06 {
+    public static void main(String[] args)
+    {
+        int starCount = (int)(Math.random() * 6.0) + 2; // 1) 2~7사이 랜덤값
+      //  Scanner scan = new Scanner(System.in);
+      //  System.out.print("별의 갯수를 입력해주세요.:");
+        //int Star_num = scan.nextInt();
+        int _divide = 0;
+        String _star="*";
+/*
+        for (int i = 0 ; i<=Star_num; i++ )
+        {
+            for (int j =0; j<i;j++){
+            System.out.printf("%s",_star);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0 ; i<=Star_num; i++ )
+        {
+            for (int j =0; j<i;j++){
+                System.out.printf("%s",_star);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        */
+        for (int i=1; i<=100;i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+}
